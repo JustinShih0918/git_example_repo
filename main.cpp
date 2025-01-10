676b682a8a64be8225349618408cfd89bf63fb2c
JustinShih0918 <s0910283@my.cmsh.cyc.edu.tw>
2025-01-10 15:42:22 +0800

    Add random output to main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,9 @@
 #include <iostream>
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    // random number, if it is 1 , cout dit
+    if (rand() % 2 == 1) {
+        std::cout << "dit" << std::endl;
+    }
     return 0;
 }
